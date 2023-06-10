c459620e155abcd5cb7aaddcc230a19d843ccfa1
cheney.yin <cheneyyin@hotmail.com>
2023-06-10 14:41:21 +0800

    Init sfaker.

--- sfaker-core/src/main/scala/org/cheney/Case0.scala
+++ sfaker-core/src/main/scala/org/cheney/Case0.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.cheney
 
 import org.apache.spark.sql.SparkSession
@@ -8,7 +25,9 @@ import org.cheney.fake.FakeSourceProps
 object Case0 {
   def main(args: Array[String]): Unit = {
     val startTime = System.currentTimeMillis();
-    val spark = SparkSession.builder().master("local[*]")
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
       .appName("Case0")
       .getOrCreate();
 
@@ -27,19 +46,33 @@ object Case0 {
     val schema = new StructType()
       .add("id", DataTypes.IntegerType)
       .add("sex", DataTypes.BooleanType)
-      .add("roles", DataTypes.createArrayType(
-        DataTypes.createMapType(DataTypes.StringType, DataTypes.createStructType(
-          new StructType().add("score", DataTypes.IntegerType)
+      .add(
+        "roles",
+        DataTypes.createArrayType(
+          DataTypes.createMapType(
+            DataTypes.StringType,
+            DataTypes.createStructType(
+              new StructType()
+                .add("score", DataTypes.IntegerType)
+                .add("class", DataTypes.StringType)
+                .fields
+            )
+          )
+        )
+      )
+      .add(
+        "conf",
+        DataTypes.createMapType(DataTypes.StringType, DataTypes.IntegerType)
+      )
+      .add(
+        "info",
+        DataTypes.createStructType(
+          new StructType()
+            .add("score", DataTypes.IntegerType)
             .add("class", DataTypes.StringType)
             .fields
-        ))
-      ))
-      .add("conf", DataTypes.createMapType(DataTypes.StringType, DataTypes.IntegerType))
-      .add("info", DataTypes.createStructType(
-        new StructType().add("score", DataTypes.IntegerType)
-          .add("class", DataTypes.StringType)
-          .fields
-      ));
+        )
+      );
 
     val unsafeRowEnable = true;
     val codegenEnable = false;
--- sfaker-core/src/main/scala/org/cheney/Case2.scala
+++ sfaker-core/src/main/scala/org/cheney/Case2.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.cheney
 
 import org.apache.spark.sql.catalyst.expressions.codegen.UnsafeRowWriter
@@ -13,16 +30,21 @@ object Case2 {
       .add("name", DataTypes.StringType)
       .add("roles", DataTypes.createArrayType(DataTypes.StringType))
 //      //      .add("roles", DataTypes.createArrayType(DataTypes.createArrayType(DataTypes.IntegerType)))
-      .add("conf", DataTypes.createMapType(DataTypes.StringType, DataTypes.IntegerType))
-      .add("info", DataTypes.createStructType(
-        new StructType().add("score", DataTypes.IntegerType)
-          .add("class", DataTypes.StringType)
-          .fields
-      ))
-     ;
+      .add(
+        "conf",
+        DataTypes.createMapType(DataTypes.StringType, DataTypes.IntegerType)
+      )
+      .add(
+        "info",
+        DataTypes.createStructType(
+          new StructType()
+            .add("score", DataTypes.IntegerType)
+            .add("class", DataTypes.StringType)
+            .fields
+        )
+      );
 
     val generator = new FakeUnsafeGenerator(schema, true);
-
 
     val row = generator.init().gen();
     println(row.getInt(0));
